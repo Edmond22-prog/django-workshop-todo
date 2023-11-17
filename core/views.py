a2a6c03715a1e17da197c8425612df9601d37456
Edghi <edghimakoll@gmail.com>
2023-11-17 01:10:33 +0100

    Last template for sure

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "core/index.html")
+
+
+def test(request):
+    return HttpResponse("Hello World!")
